5dabec0db4f5e6322238e5bbf8de4de83334644c
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2017-05-03 10:34:50 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -7,18 +7,24 @@ class App extends React.Component {
 			txt: 'this is the state txt'
 		}
 	}
+	//updates the render simultaneously, and it gets outputed in render ()
 	update ( e ) {
 		this.setState({txt: e.target.value})
 	} 
 	render(){
 		return (
 		<div>
-			<input type="text"
-			onChange={this.update.bind(this)}/>
 			<h1>{this.state.txt}</h1>
+			<Widget update={this.update.bind(this)} />
+			<Widget update={this.update.bind(this)} />
+			<Widget update={this.update.bind(this)} />
+			<Widget update={this.update.bind(this)} />
+
 		</div>
 	)
 	}
 }
+const Widget = (props) => 
+	<input type="text" onChange={props.update }/>
 
 export default App
